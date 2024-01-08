39903e6fc6bfa9b09abbe5e60c4fabba8909b8ea
Hanna Joo <69278218+hanna-joo@users.noreply.github.com>
2024-01-08 18:35:50 +0900

    Title: 순위 경쟁, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240108/순위 경쟁/ranking-competition.py
@@ -0,0 +1,14 @@
+# 명예의 전당에 올라간 사람의 조합의 변경 횟수
+scores = {'A': 0, 'B': 0, 'C': 0}
+fame = ['A', 'B', 'C']
+highest = 0
+ans = 0
+for _ in range(int(input())):
+    c, s = input().split()
+    scores[c] += int(s)
+    if scores[c] >= highest:
+        new_fame = [k for k, v in scores.items() if v >= scores[c]]
+        if new_fame != fame:
+            fame = new_fame
+            ans += 1
+print(ans)
