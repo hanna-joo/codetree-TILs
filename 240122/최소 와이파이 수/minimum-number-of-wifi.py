2e947efaa6d018e09d67ef339d0c6c9b123aa872
Hanna Joo <69278218+hanna-joo@users.noreply.github.com>
2024-01-22 21:52:56 +0900

    Title: 최소 와이파이 수, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

--- 240122/최소 와이파이 수/minimum-number-of-wifi.py
+++ 240122/최소 와이파이 수/minimum-number-of-wifi.py
@@ -10,35 +10,46 @@
     - 1<=n<=100
     - 1<=m<=100
 
-3. 로직
+3. 로직1
 - 와이파이를 최대한 오른쪽에 설치하는 것이 좋음
-    - 사람이 있으면 그 때부터 오른쪽으로 갈 때마다 카운트
-    - 카운트가 m이 되면 와이파이 1개 추가
+- 사람이 있으면 그 때부터 오른쪽으로 갈 때마다 카운트
+- 카운트가 m이 되면 와이파이 1개 추가
+
+4. 로직2
+- 사람이 있으면 현재+m 위치에 와이파이 설치
+- 현재+m+m 위치에서 다시 탐색 시작
 """
 
 n, m = map(int, input().split())
 houses = [*map(int, input().split())]
-ready = False
-cnt = 0
-pos = 0
-ans = 0
 
-# if m >= n and 1 in houses:
-#     print(1)
-#     exit()
-
+ans, pos = 0, 0
 while pos < n:
     if houses[pos] == 1:
-        ready = True
-    if ready:
-        if cnt == m:
-            ans += 1
-            ready, cnt = False, 0
-            pos += max(1, m+1)
-            continue
-        cnt += 1
-    pos += 1
-
-if ready:
-    ans += 1
+        ans += 1
+        pos += 2 * m + 1
+    else:
+        pos += 1
 print(ans)
+
+
+
+# 로직1
+# cnt, pos, ready = 0, 0, False
+# ans = 0
+
+# while pos < n:
+#     if houses[pos] == 1:
+#         ready = True
+#     if ready:
+#         if cnt == m:
+#             ans += 1
+#             ready, cnt = False, 0
+#             pos += max(1, m+1)
+#             continue
+#         cnt += 1
+#     pos += 1
+
+# if ready:
+#     ans += 1
+# print(ans)
