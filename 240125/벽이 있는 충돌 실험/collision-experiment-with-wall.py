e35082ddae8173001e65d1b2d8d2427dfda07226
Hanna Joo <69278218+hanna-joo@users.noreply.github.com>
2024-01-25 18:05:14 +0900

    Title: 벽이 있는 충돌 실험, Time: 5094ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

---
+++ 240125/벽이 있는 충돌 실험/collision-experiment-with-wall.py
@@ -0,0 +1,63 @@
+"""
+1. 문제
+- 시간 소요 : 1초에 한 칸 이동
+- 방향 전환 : 벽에 부딪히면 반대 방향으로 전환
+- 구슬 삭제 : 이동 이후 한 칸에 구슬이 2개 이상인 경우
+- return : T줄에 걸쳐 해당 테스트 케이스에 남아 있는 구슬의 수 출력
+
+2. 입력 및 제한
+- 테스트 케이스 개수 T (1<=T<=100)
+- 각 테스트 케이스에 대한 정보
+    - N, M (1<=N<=50, 0<=M<=N*N)
+    - M개의 줄에 걸쳐 구슬 위치인 행, 열, 방향 (1<=x<=N, 1<=y<=N, d=[U,D,R,L])
+
+3. 로직
+- 반복 : 구슬 이동 -> 2개 이상인 칸은 구슬 없애기 -> 구슬 개수 세기 -> 종료 여부 확인
+- 종료 조건
+    - 구슬의 개수가 0 / 1개일 때 종료
+    - 구슬이 제자리로 올 때까지 구슬 개수가 그대로면 종료 (2*N+2초 뒤)
+"""
+
+T = int(input())
+command = {'U': (-1, 0), 'D': (1, 0), 'L': (0, -1), 'R': (0, 1)}
+change = {'U': 'D', 'D': 'U', 'L': 'R', 'R': 'L'}
+for _ in range(T):
+    N, M = map(int, input().split())
+    ans = N
+    balls_pos = []
+    balls_cnt = [[0 for _ in range(N+1)] for _ in range(N+1)]
+    for _ in range(M):
+        x, y, d = input().split()
+        x, y = int(x), int(y)
+        balls_pos.append([x, y, d])
+        balls_cnt[x][y] += 1
+    while True:
+        tmp_pos = []
+        for cx, cy, cd in balls_pos:
+            nx, ny = cx + command[cd][0], cy + command[cd][1]
+            # 다음 위치가 벽이라면 방향 전환 및 다시 다음 좌표 잡기
+            if nx <= 0 or nx > N or ny <= 0 or ny > N:
+                cd = change[cd]
+                nx, ny = cx + command[cd][0], cy + command[cd][1]
+            # 구슬 이동
+            balls_cnt[cx][cy] -= 1
+            balls_cnt[nx][ny] += 1
+            # 새로운 위치 저장
+            tmp_pos.append([nx, ny, cd])
+        total = 0
+        for i in range(N+1):
+            for j in range(N+1):
+                if balls_cnt[i][j] >= 2:
+                    balls_cnt[i][j] = 0
+                    tmp_pos_2 = []
+                    for i, k in enumerate(tmp_pos):
+                        if k[:2] == [i, j]:
+                            continue
+                        tmp_pos_2.append(k)
+                    continue
+                total += balls_cnt[i][j]
+        if total in [0, 1]:
+            ans = total
+            break
+        balls_pos = tmp_pos
+    print(ans)
