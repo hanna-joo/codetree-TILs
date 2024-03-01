e3e4299ea54cfc7fa7fd79cdb7a9f54a1ea9a916
Hanna Joo <69278218+hanna-joo@users.noreply.github.com>
2024-03-01 11:44:15 +0900

    Title: 모이자, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240301/모이자/gather.py
@@ -0,0 +1,35 @@
+"""
+1. 문제
+- return : n개의 집 중 한 곳에 전부 모일 때 사람들의 이동 거리 합 중 최솟값 출력
+
+2. 입력 및 제한
+- n (1 <= n <= 100)
+- 각 지점에 살고 있는 사람 수에 해당하는 n개의 숫자 주어짐 (1 <= Ai <= 100)
+
+3. solution : 완전탐색
+- 0번 집에 모일 때 = (0번 사람 수 * 0) + (1번 사람 수 * 1) + ... + (4번 사람 수 * 4)
+- 1번 집에 모일 때 = (0번 사람 수 * 1) + (1번 사람 수 * 0) + ... + (4번 사람 수 * 3)
+"""
+
+import sys
+
+INT_MAX = sys.maxsize
+
+n = int(input())
+houses = list(map(int, input().split()))
+ans = INT_MAX
+
+
+def calculate_distance(num):
+    dist = 0
+    for i in range(n):
+        dist += houses[i] * abs(num - i)
+    return dist
+
+
+
+for i in range(n):
+    ans = min(ans, calculate_distance(i))
+
+
+print(ans)
