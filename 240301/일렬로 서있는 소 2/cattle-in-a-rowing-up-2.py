4137421faec444f93dfe165ac4bb0b46a6a1fad4
Hanna Joo <69278218+hanna-joo@users.noreply.github.com>
2024-03-01 12:10:27 +0900

    Title: 일렬로 서있는 소 2, Time: 150ms, Memory: 25MB, Status: Passed - CodeTree

--- 240301/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240301/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -13,13 +13,13 @@
 """
 
 N = int(input())
-heights = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
 cnt = 0
 for i in range(N):
     for j in range(i+1, N):
         for k in range(j+1, N):
-            if heights[i] <= heights[j] <= heights[k]:
+            if arr[i] <= arr[j] <= arr[k]:
                 cnt += 1
 
 print(cnt)
